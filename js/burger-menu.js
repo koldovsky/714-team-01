5fd4deb491bc4b5df1ccdbce502512f65daceb4d
Anatolii Horobets <admin@cheapsite.studio>
2022-09-28 16:08:14 +0300

    media for mobile menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -5,17 +5,3 @@ menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("active");
   menuOverlay.classList.toggle("open");
 });
-
-/* window.onscroll = function () {
-  transformLogo();
-};
-
-const logo = document.querySelector(".logo");
-
-function transformLogo() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    logo.classList.add("scrolled");
-  } else {
-    logo.classList.remove("scrolled");
-  }
-} */
