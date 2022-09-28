2dc6fe5cdabd866d5f84d966a88feaff087fea63
Dmytro <geg.barinov@gmail.com>
2022-09-28 13:16:40 +0300

    code format fixed

--- js/breed-selector.js
+++ js/breed-selector.js
@@ -1,40 +1,39 @@
-(function() {
+(function () {
     const breeds = [
-        {
-            id: 1,
-            name: 'Siberian Husky',
-            description: 'A beautiful and active breed with a thick coat and blue eyes.',
-            class: '-first'
-        },
-        {
-            id: 2,
-            name: 'Alaskan Husky',
-            description: 'Good-natured and energetic dogs for friendly families.',
-            class: '-second'
-        },
-        {
-            id: 3,
-            name: 'Labrador Husky',
-            description: 'An unusual mixed breed with blue eyes and a harsh appearance.',
-            class: '-third'
-        }
+      {
+        id: 1,
+        name: "Siberian Husky",
+        description:
+          "A beautiful and active breed with a thick coat and blue eyes.",
+        class: "-first",
+      },
+      {
+        id: 2,
+        name: "Alaskan Husky",
+        description: "Good-natured and energetic dogs for friendly families.",
+        class: "-second",
+      },
+      {
+        id: 3,
+        name: "Labrador Husky",
+        description:
+          "An unusual mixed breed with blue eyes and a harsh appearance.",
+        class: "-third",
+      },
     ];
-    
+  
     function rendBreeds(breeds) {
-        const breedsWrapper = document.querySelector('.huski__breeds-selector');
-        breedsWrapper.innerHTML = "";
-        for (const breed of breeds) {
-            breedsWrapper.innerHTML += `
-                    <div class="breed-selector-card breed-selector-card${breed.class}">
-                        <h3>${breed.name}</h3>
-                        <p>${breed.description}</p>
-                        <a href="#dogs">CHOOSE A PUPPY</a>
-                    </div>                    
-            `;
-        }
+      const breedsWrapper = document.querySelector(".huski__breeds-selector");
+      breedsWrapper.innerHTML = "";
+      for (const breed of breeds) {
+        breedsWrapper.innerHTML += `
+                      <div class="breed-selector-card breed-selector-card${breed.class}">
+                          <h3>${breed.name}</h3>
+                          <p>${breed.description}</p>
+                          <a href="#dogs">CHOOSE A PUPPY</a>
+                      </div>                    
+              `;
+      }
     }
     rendBreeds(breeds);
-
-
-
-})();
+  })();
