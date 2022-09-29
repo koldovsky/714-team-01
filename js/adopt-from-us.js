07398008f287828d3cf79407828c1ac7666b143d
Elena <elena.brovko.01@gmail.com>
2022-09-29 23:41:36 +0300

    changed adopt-from-us.js for clock

--- js/adopt-from-us.js
+++ js/adopt-from-us.js
@@ -1,8 +1,9 @@
 const clockAdoptFromUs = document.querySelector('.adopt-from-us__clock');
-clockAdoptFromUs.innerHTML = new Date().toLocaleDateString();
+clockAdoptFromUs.innerHTML = new Date().toLocaleTimeString();
 
 function updateAdoptFromUsClock(){
     clockAdoptFromUs.innerHTML = new Date().toLocaleTimeString();
 }
 
 setInterval(updateAdoptFromUsClock,1000);
+
