232ba4da3de1897f919965e45a23fca017ba38da
Anatolii Horobets <admin@cheapsite.studio>
2022-09-30 11:55:55 +0300

    reset burger menu

--- js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector(".menu-button");
-const menuOverlay = document.querySelector(".menu-overlay");
-
-menuButton.addEventListener("click", function () {
-  menuButton.classList.toggle("active");
-  menuOverlay.classList.toggle("open");
-});
