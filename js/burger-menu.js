b0a6a782e71fda8719cffba354213eda0aecf254
Anatolii Horobets <admin@cheapsite.studio>
2022-10-03 12:01:04 +0300

    added code to burger-menu.js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,15 +1,16 @@
 (() => {
   const menuButton = document.querySelector(".menu-button");
   const menuOverlay = document.querySelector(".menu-overlay");
+  const items = document.querySelectorAll('.nav__item');
 
   menuButton.addEventListener("click", function () {
     menuButton.classList.toggle("active");
     menuOverlay.classList.toggle("open");
   });
 
-  nav__item.addEventListener("click", function () {
+  items.forEach( navItem => {
     menuButton.classList.remove("active");
     menuOverlay.classList.remove("open");
-  });
+});
 
 })();
