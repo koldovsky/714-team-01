4380606ff6d65b2e928b8a7fa37a400307366611
Anatolii Horobets <admin@cheapsite.studio>
2022-10-03 09:42:36 +0300

    added code to burger menu (not work)

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,11 +1,15 @@
 (() => {
+  const menuButton = document.querySelector(".menu-button");
+  const menuOverlay = document.querySelector(".menu-overlay");
 
-const menuButton = document.querySelector(".menu-button");
-const menuOverlay = document.querySelector(".menu-overlay");
+  menuButton.addEventListener("click", function () {
+    menuButton.classList.toggle("active");
+    menuOverlay.classList.toggle("open");
+  });
 
-menuButton.addEventListener("click", function () {
-  menuButton.classList.toggle("active");
-  menuOverlay.classList.toggle("open");
-});
+  nav__item.addEventListener("click", function () {
+    menuButton.classList.remove("active");
+    menuOverlay.classList.remove("open");
+  });
 
 })();
