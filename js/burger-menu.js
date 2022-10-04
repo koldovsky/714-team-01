c8318e5a320756cefc32f53ef19b13a3c9b2be9f
Anatolii Horobets <admin@cheapsite.studio>
2022-10-04 12:15:17 +0300

    added close effect to burger menu after click

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,16 +1,17 @@
 (() => {
   const menuButton = document.querySelector(".menu-button");
   const menuOverlay = document.querySelector(".menu-overlay");
-  const items = document.querySelectorAll('.nav__item');
+  const items = document.querySelectorAll(".nav__item");
 
   menuButton.addEventListener("click", function () {
-    menuButton.classList.toggle("active");
-    menuOverlay.classList.toggle("open");
+      menuButton.classList.toggle("active");
+      menuOverlay.classList.toggle("open");
   });
 
-  items.forEach( navItem => {
-    menuButton.classList.remove("active");
-    menuOverlay.classList.remove("open");
-});
-
+  items.forEach((navItem) => {
+      navItem.addEventListener("click", () => {
+          menuButton.classList.remove("active");
+          menuOverlay.classList.remove("open");
+      });
+  });
 })();
