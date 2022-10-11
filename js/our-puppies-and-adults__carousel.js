24ce143570da56cbaedafd27b72eb3e01a577ecb
DariaBV <dreamhappy8@gmail.com>
2022-10-11 03:16:53 +0000

    Fixed JS to section OUR-PUPPIES-AND-ADULTS

--- js/our-puppies-and-adults__carousel.js
+++ js/our-puppies-and-adults__carousel.js
@@ -1,22 +1,21 @@
-(function() {
+(function () {
 
     const slides = [
-            '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-male.png" alt="Gray husky"><p class="our-puppies-and-adults__image-text">Donnie, male</p>',
-            '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-puppy-female.png" alt="Hasky puppy is lying down."><p class="our-puppies-and-adults__image-text">Silva, female</p></div>',
-            '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-puppy-male.png" alt="Hasky puppy eats from a bowl."><p class="our-puppies-and-adults__image-text">Miska, male</p></div>',
-            '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-female.png" alt="Red husky"><p class="our-puppies-and-adults__image-text">Ulva, male</p></div>'
+        '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-male.png" alt="Gray husky"><p class="our-puppies-and-adults__image-text">Donnie, male</p>',
+        '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-puppy-female.png" alt="Hasky puppy is lying down."><p class="our-puppies-and-adults__image-text">Silva, female</p></div>',
+        '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-puppy-male.png" alt="Hasky puppy eats from a bowl."><p class="our-puppies-and-adults__image-text">Miska, male</p></div>',
+        '<div class="our-puppies-and-adults__cart-containar"><img class="our-puppies-and-adults__image" src="img/husky-female.png" alt="Red husky"><p class="our-puppies-and-adults__image-text">Ulva, male</p></div>'
     ];
 
     let currentSlideIdx = 0;
 
     const slideContainer = document.querySelector('.our-puppies-and-adults__carousel .our-puppies-and-adults__carousel-slides');
-    const dotsContainer = document.querySelector('.our-puppies-and-adults__indicators .indicators-dot');
 
     function renderSlide() {
         slideContainer.innerHTML = slides[currentSlideIdx];
-    
+
         if (window.innerWidth > 768) {
-            const secondSlideIdx = currentSlideIdx + 1>= slides.length ? 0 : currentSlideIdx + 1;
+            const secondSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
             slideContainer.innerHTML += slides[secondSlideIdx];
         }
     }
@@ -27,7 +26,7 @@
     }
 
     function prevSlide() {
-        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1 ;
+        currentSlideIdx = currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
         renderSlide();
     }
 
@@ -40,5 +39,19 @@
     btnPrev.addEventListener('click', prevSlide);
 
     window.addEventListener('resize', renderSlide);
+
+    const dotsNav = document.querySelector('.our-puppies-and-adults__indicators');
+    const dots = Array.from(dotsNav.children);
+
+    dotsNav.addEventListener('click', e => {
+        const targetDot = e.target.closest('button');
+
+        if (!targetDot) return;
+
+        const targetIndex = dots.findIndex(dot => dot === targetDot);
+        currentSlideIdx = targetIndex;
+
+        renderSlide();
+    });
 
 })();
